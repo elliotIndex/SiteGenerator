f0d768e738d21b0927ba6e620d058bdb3d82d4ca
Elliot Plant <elliotaplant@gmail.com>
2017-05-07 13:19:40 -0700

    Get path to buld

--- wrapper/js/utils.js
+++ wrapper/js/utils.js
@@ -1,12 +1,22 @@
 function deepSet(obj, path, value) {
   path = path.split('.');
-  console.log("moo");
+
+  // If at the end of the path, assign the value
   if (path.length === 1) {
     obj[path[0]] = value;
-  } else {
-    obj[path[0]] = obj[path[0]] || {};
-    deepSet(obj[path[0]], path.slice(1).join('.'), value)
+    return obj;
   }
+
+  // If next item in path is a number, then current item should be an array
+  if (path[1] && !Number.isNaN(+path[1])) {
+    obj[path[0]] = obj[path[0]] || [];
+  } else {
+    // Else, the next item should be an object
+    obj[path[0]] = obj[path[0]] || {};
+  }
+
+  // Recurse down the tree with the next depth and next path
+  deepSet(obj[path[0]], path.slice(1).join('.'), value);
   return obj;
 }
 
