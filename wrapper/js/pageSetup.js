f398726af8b1a8dfe0e3499d6c9f78dacccfd2fc
Elliot Plant <elliotaplant@gmail.com>
2017-05-07 13:09:03 -0700

    Make object from template

--- wrapper/js/pageSetup.js
+++ wrapper/js/pageSetup.js
@@ -1,33 +1,20 @@
 var utils = require('./utils');
+var save = require('./save');
 
 function mobilize() {
   console.log('togging');
   $('.template').toggleClass('mobile');
 }
 
-
-var save = function() {
-  console.log("not ready yet");
+var templateFrameLoad = function() {
+  console.log('iframe loaded');
+  var templateItems = $('#template-frame').contents().find('[data-template-path]');
+  templateItems.attr('contenteditable', 'true');
+  $('#save-btn').on('click', function() { save(templateItems); });
 };
 
-var templateFrameLoad = function() {
-  console.log("iframe loaded");
-  var innerDoc = $('#template-frame').contents();
-  innerDoc.find('[data-template-path]').attr('contenteditable', 'true');
-  $('#save-btn').off('click', save);
-  $('#save-btn').on('click', function() {
-    console.log('saving');
-    var selectables = Array.from(innerDoc.find('[data-template-path]')).map(selectable => ({
-      path: selectable.dataset.templatePath,
-      text: selectable.textContent.trim()
-    }));
-    console.log('selectables', selectables);
-    var json = deepSet({}, 'moop', 'poop')
-  });
-}
 
 module.exports = function() {
-  $('#save-btn').click(save);
   $('#mobile-toggle-btn').click(mobilize);
   $('#template-frame').on('load', templateFrameLoad);
 }
---
+++ wrapper/js/save.js
@@ -0,0 +1,17 @@
+var utils = require('./utils');
+
+module.exports = function(templateItems) {
+  console.log('saving');
+  var parsedTemplateItems = Array.from(templateItems)
+    .map(function(templateItem) {
+      return {
+        path: templateItem.dataset.templatePath,
+        text: templateItem.textContent.trim()
+      };
+    });
+
+  var output = parsedTemplateItems.reduce(function(out, templateItem) {
+    return utils.deepSet(out, templateItem.path, templateItem.text);
+  }, {});
+  console.log("output", output);
+}
